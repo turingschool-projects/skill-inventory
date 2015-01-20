384a9a6b94d2279be0f8f6745749e5c4670d1c15
Ian Andersen <ianderse@mac.com>
2015-01-20 15:52:38 -0700

    sessions controller and services setup for github authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   belongs_to :cohort
+
+  before_create :generate_token
+
+  protected
+
+  def generate_token
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless User.exists?(token: random_token)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :skills, only: [:index, :create], defaults: { format: :json }
       resources :groups, only: [:index], defaults: { format: :json }
+      resources :sessions, only: [:create]
     end
   end
 end
