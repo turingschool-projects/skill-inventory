6f5d0dd9f6df083cef30cc1a44b6a1e234e97186
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-01-21 08:02:43 -0700

    dbl quote string

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ FactoryGirl.create(:skill, name: "skill2", featured: false)
 FactoryGirl.create(:skill, name: "skill2", featured: true)
 FactoryGirl.create(:skill, name: "skill2", featured: false)
 
-puts " Successfully added skills from seed data. ".center(70, "~*~')
+puts " Successfully added skills from seed data. ".center(70, "~*~")
 
 FactoryGirl.create(:group, name: "1407")
 FactoryGirl.create(:group, name: "1409")
