5b7708b1d3af0d58992dd68edf7585d673a49454
Kavita <ks@kavitasa.com>
2015-01-26 13:52:25 -0700

    update user test to reflect removal of predicate methods from user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,7 @@ describe User do
   it "sets default role to student" do
     user = User.new
 
-    expect(user.student?).to be_truthy
-  end
-
-  it "knows if it is an instructor" do
-    user = User.new
-    user.role = "instructor"
-
-    expect(user.instructor?).to be_truthy
+    expect(user.role).to eq("student")
   end
 
 end
