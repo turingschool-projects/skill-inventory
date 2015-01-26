f266d03343dce79fcf8a7607416bace865c9f6c7
Kavita <ks@kavitasa.com>
2015-01-26 14:09:02 -0700

    WIP add skill and tag associations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,7 @@
 class Skill < ActiveRecord::Base
   belongs_to :group
+  has_many :skill_tags
+  has_many :tags, through: :skill_tags
 
   validates :name, presence: true
   validates :featured, inclusion: [true, false]
