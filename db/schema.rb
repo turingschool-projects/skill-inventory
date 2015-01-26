af4ab7e298d8d4992c7cd6fd0757cf01f2c81f65
Kavita <ks@kavitasa.com>
2015-01-26 13:34:18 -0700

    add default role of student to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120221115) do
+ActiveRecord::Schema.define(version: 20150126202759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150120221115) do
 
   create_table "users", force: :cascade do |t|
     t.string  "name"
-    t.string  "role"
+    t.string  "role",     default: "student"
     t.integer "group_id"
     t.string  "token"
   end
