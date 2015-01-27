7b72e0762cfb1ce20c21480d8dcb7e820ef6e215
Luke Aiken <lukeaiken@gmail.com>
2015-01-27 12:49:50 -0700

    using id but not in strong params

--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::SkillsController < Api::V1::BaseController
 
   def create
     skill = Skill.new(skill_params)
+    skill.section = Section.where(id: params[:skill][:section]).first
 
     if skill.save
       render status: 201, json: skill, root: "skill"
@@ -23,6 +24,7 @@ class Api::V1::SkillsController < Api::V1::BaseController
 
   def update
     skill = Skill.find(params[:id])
+    skill.section = Section.where(id: params[:skill][:section]).first
 
     if skill.update_attributes(skill_params)
       render status: 200, json: skill, root: "skill"
@@ -47,6 +49,6 @@ class Api::V1::SkillsController < Api::V1::BaseController
   end
 
   def skill_params
-    params.require(:skill).permit(:name, :featured, :section_id)
+    params.require(:skill).permit(:name, :featured)
   end
 end
--- spec/controllers/api/v1/skills_controller_spec.rb
+++ spec/controllers/api/v1/skills_controller_spec.rb
@@ -27,7 +27,7 @@ describe Api::V1::SkillsController do
     it "creates a skill (with full parameters)" do
       section = create(:section)
       skill_attributes = attributes_for(:skill)
-      skill_attributes[:section_id] = section.id
+      skill_attributes[:section] = section.id
 
       post :create, format: :json, skill: skill_attributes
 
@@ -71,7 +71,7 @@ describe Api::V1::SkillsController do
                    skill: {
                             name: "after_updated_name",
                             featured: true,
-                            section_id: section_2.id
+                            section: section_2.id
                           }
 
       json_response = JsonResponse.new(response)
@@ -88,7 +88,8 @@ describe Api::V1::SkillsController do
 
       json_response = JsonResponse.new(response)
       expect(json_response.headers).to have_http_status(:unprocessable_entity)
-      expect(json_response.error_message).to eq(["Name can't be blank"])
+      expect(json_response.error_message).to eq(["Name can't be blank",
+                                                "Section can't be blank"])
     end
   end
 
