8352f6c9bd04bbff9b28fc3cfc4e39eac64b5a56
Kavita <ks@kavitasa.com>
2015-01-27 09:47:30 -0700

    update sections controller test with multiple sections

--- spec/controllers/api/v1/sections_controller_spec.rb
+++ spec/controllers/api/v1/sections_controller_spec.rb
@@ -3,13 +3,14 @@ describe Api::V1::SectionsController do
   describe "index" do
 
     it "returns a json array of all sections" do
-      section = create(:section)
+      section1 = create(:section)
+      section2 = create(:section)
 
       get :index, format: :json
 
       json_response = JsonResponse.new(response)
       expect(json_response.headers).to have_http_status(:ok)
-      expect(json_response.section_name).to eq(section.name)
+      expect(json_response.section_name).to eq(section2.name)
     end
   end
 end
