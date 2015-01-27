7fdb052b5d9afd1f38a326af5e420da97a8f9589
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-01-27 11:19:57 -0700

    update with upstream. ratings controller tests commented out

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,17 +1,16 @@
 describe Rating do
 
   it "is valid" do
-    user = create(:user)
-    skill = create(:skill)
+    user   = create(:user)
+    skill  = create(:skill)
     rating = create(:rating, user_id: user.id, skill_id: skill.id)
 
     expect(rating).to be_valid
   end
 
   it "is invalid without a user_id" do
-    user = create(:user)
-    rating = create(:rating, user_id: "")
+    rating = build(:rating, user_id: "")
 
-    expect(rating).to_not be_valid
+    expect(rating).to be_invalid
   end
 end
