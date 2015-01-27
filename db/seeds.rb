7c3513ecc85aff80007d043a19c030fc957feaf1
Ian Andersen <ianderse@mac.com>
2015-01-27 11:25:03 -0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 require "factory_girl_rails"
 
-FactoryGirl.create(:section, name: "Module 1")
-FactoryGirl.create(:section, name: "Module 2")
-FactoryGirl.create(:section, name: "Module 3")
-FactoryGirl.create(:section, name: "Module 4")
+m1 = FactoryGirl.create(:section, name: "Module 1")
+m2 = FactoryGirl.create(:section, name: "Module 2")
+m3 = FactoryGirl.create(:section, name: "Module 3")
+m4 = FactoryGirl.create(:section, name: "Module 4")
 
 puts " Successfully added sections from seed data. ".center(70, "~*~")
 
-FactoryGirl.create(:skill, name: "skill2", featured: true)
-FactoryGirl.create(:skill, name: "skill2", featured: true)
-FactoryGirl.create(:skill, name: "skill2", featured: false)
-FactoryGirl.create(:skill, name: "skill2", featured: true)
-FactoryGirl.create(:skill, name: "skill2", featured: false)
+FactoryGirl.create(:skill, name: "skill2", featured: true, section: m1)
+FactoryGirl.create(:skill, name: "skill2", featured: true, section: m2)
+FactoryGirl.create(:skill, name: "skill2", featured: false, section: m3)
+FactoryGirl.create(:skill, name: "skill2", featured: true, section: m4)
+FactoryGirl.create(:skill, name: "skill2", featured: false, section: m1)
 
 puts " Successfully added skills from seed data. ".center(70, "~*~")
