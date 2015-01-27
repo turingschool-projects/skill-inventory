fbd0cff1af226f98e9eec85c919b09c804c740de
Ian Andersen <ianderse@mac.com>
2015-01-27 10:06:18 -0700

    add show method to section controller

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::SectionsController < Api::V1::BaseController
   def index
     render json: { section: Section.all }
   end
+
+  def show
+    section = Section.find(params[:id])
+    render json: { section: section }
+  end
 end
