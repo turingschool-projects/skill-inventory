b13eaffb8c8081887d41f1921f671223619540e8
Steve Kinney <hello@stevekinney.net>
2015-01-30 12:36:22 -0700

    Write test for RatingsController#create

--- spec/controllers/api/v1/ratings_controller_spec.rb
+++ spec/controllers/api/v1/ratings_controller_spec.rb
@@ -22,4 +22,16 @@ describe Api::V1::RatingsController do
       expect(json_response.rating_id).to eq(rating.id)
     end
   end
+
+  describe "create" do
+    it "creates a rating" do
+      user = create(:user)
+      skill = create(:skill)
+      allow_any_instance_of(Api::V1::BaseController).to receive(:current_user).and_return(user)
+
+      expect {
+        post :create, format: :json, rating: { "skill" => skill.id, "score" => 1 }
+       }.to change{ Rating.count }.by(1)
+    end
+  end
 end
