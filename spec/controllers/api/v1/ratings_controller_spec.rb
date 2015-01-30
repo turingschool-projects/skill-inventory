4825e47e9a38868b1c52a16154f5a07fd7da1f84
Steve Kinney <hello@stevekinney.net>
2015-01-30 11:20:09 -0700

    Clean up ratings_controller_spec

--- spec/controllers/api/v1/ratings_controller_spec.rb
+++ spec/controllers/api/v1/ratings_controller_spec.rb
@@ -1,5 +1,4 @@
 describe Api::V1::RatingsController do
-
   describe "index" do
     it "returns a json array of all ratings" do
       rating = create(:rating)
@@ -14,13 +13,13 @@ describe Api::V1::RatingsController do
 
   describe "show" do
     it "returns a rating" do
-    rating = create(:rating)
+      rating = create(:rating)
 
-    get :show, format: :json, id: rating.id
+      get :show, format: :json, id: rating.id
 
-    json_response = JsonResponse.new(response)
-    expect(json_response.headers).to have_http_status(:ok)
-    expect(json_response.rating_id).to eq(rating.id)
+      json_response = JsonResponse.new(response)
+      expect(json_response.headers).to have_http_status(:ok)
+      expect(json_response.rating_id).to eq(rating.id)
     end
   end
 end
