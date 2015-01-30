933916fc090af26832cf88d3b97dd1a154f1df24
Steve Kinney <hello@stevekinney.net>
2015-01-30 13:04:56 -0700

    Add validators and validation
    One of these needs to go, obviously.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,11 @@
+class RatingValidator < ActiveModel::Validator
+  def validate(record)
+    if Rating.where(user: record.user, skill: record.skill).first
+      record.errors[:base] << "Rating already exists for this user and skill."
+    end
+  end
+end
+
 class Rating < ActiveRecord::Base
   belongs_to :skill
   belongs_to :user
@@ -5,4 +13,13 @@ class Rating < ActiveRecord::Base
   validates :user, presence: true
   validates :skill, presence: true
   validates :score, inclusion: { in: 0..4 }
+  validate :cannot_have_the_same_user_and_skill_as_another_rating
+
+  private
+
+  def cannot_have_the_same_user_and_skill_as_another_rating
+    if self.class.where(user: user, skill: skill).first
+      errors[:base] << "Rating already exists for this user and skill."
+    end
+  end
 end
