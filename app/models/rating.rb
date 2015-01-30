4bf6309546e4c5427c631d0e45824fad2c9000d7
Josh Mejia <joshua.mejia@gmail.com>
2015-01-30 14:19:43 -0700

    Replace uniqueness rating validation.
    * Removed custom validator in favor of uniqueness
      validation on :user_id with a scope on :skill_id

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,11 +1,3 @@
-class RatingValidator < ActiveModel::Validator
-  def validate(record)
-    if Rating.where(user: record.user, skill: record.skill).first
-      record.errors[:base] << "Rating already exists for this user and skill."
-    end
-  end
-end
-
 class Rating < ActiveRecord::Base
   belongs_to :skill
   belongs_to :user
@@ -13,13 +5,5 @@ class Rating < ActiveRecord::Base
   validates :user, presence: true
   validates :skill, presence: true
   validates :score, inclusion: { in: 0..4 }
-  validate :cannot_have_the_same_user_and_skill_as_another_rating
-
-  private
-
-  def cannot_have_the_same_user_and_skill_as_another_rating
-    if self.class.where(user: user, skill: skill).first
-      errors[:base] << "Rating already exists for this user and skill."
-    end
-  end
+  validates :user_id,  :uniqueness => {:scope => [:skill_id]}
 end
